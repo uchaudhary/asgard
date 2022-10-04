4fa731f623dbe6c4933ca83cf6ec43044237125c
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-10-04 17:47:33 -0500

    Add installer for Homebrew

---
+++ server/setupHomebrew.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Install Homebrew
+# Instructions from brew.sh
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt autoremove --purge -y
+
+sudo apt install build-essential procps curl file git -y
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+test -r ~/.bash_profile && echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bash_profile
+echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.profile
